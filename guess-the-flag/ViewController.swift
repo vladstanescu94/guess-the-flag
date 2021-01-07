6dc908f94ba7cb4fcfa92f2ffd903cbe45fddb44
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-07 10:46:34 +0200

    Add game logic

--- guess-the-flag/ViewController.swift
+++ guess-the-flag/ViewController.swift
@@ -52,7 +52,7 @@ class ViewController: UIViewController {
         ]
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         
         firstFlag.setImage(UIImage(named: countries[0]), for: .normal)
@@ -63,7 +63,21 @@ class ViewController: UIViewController {
         
         title = countries[correctAnswer].uppercased()
     }
-
+    @IBAction func flagTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let alertController = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(alertController, animated: true)
+    }
 
 }
 
