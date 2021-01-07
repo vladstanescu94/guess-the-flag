bc0f23b4b932ee091bca5eb712bdaec843ccdd04
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-07 09:56:02 +0200

    Add flag assets and basic storyboard setup

--- guess-the-flag/ViewController.swift
+++ guess-the-flag/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var firstFlag: UIButton!
+    @IBOutlet var secondFlag: UIButton!
+    @IBOutlet var thirdFlag: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
