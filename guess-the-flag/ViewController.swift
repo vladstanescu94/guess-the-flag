0f26a4947c682765bcb266787f6aa29c37edfc39
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-07 10:25:06 +0200

    Display correct answer in navigation bar

--- guess-the-flag/ViewController.swift
+++ guess-the-flag/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet var thirdFlag: UIButton!
     
     var countries = [String]()
+    var correctAnswer = 0
     var score = 0
     
     override func viewDidLoad() {
@@ -52,9 +53,15 @@ class ViewController: UIViewController {
     }
     
     func askQuestion() {
+        countries.shuffle()
+        
         firstFlag.setImage(UIImage(named: countries[0]), for: .normal)
         secondFlag.setImage(UIImage(named: countries[1]), for: .normal)
         thirdFlag.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        correctAnswer = Int.random(in: 0...2)
+        
+        title = countries[correctAnswer].uppercased()
     }
 
 
