5abd1f52f1577cb7799ba71e780bc74ddaaa8f7c
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-07 10:07:23 +0200

    Set images to buttons and add styling

--- guess-the-flag/ViewController.swift
+++ guess-the-flag/ViewController.swift
@@ -13,9 +13,48 @@ class ViewController: UIViewController {
     @IBOutlet var secondFlag: UIButton!
     @IBOutlet var thirdFlag: UIButton!
     
+    var countries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
+    }
+    
+    private func setupView() {
+        setupFlags()
+        askQuestion()
+    }
+    
+    private func setupFlags() {
+        firstFlag.layer.borderWidth = 1
+        secondFlag.layer.borderWidth = 1
+        thirdFlag.layer.borderWidth = 1
+        
+        firstFlag.layer.borderColor = UIColor.lightGray.cgColor
+        secondFlag.layer.borderColor = UIColor.lightGray.cgColor
+        thirdFlag.layer.borderColor = UIColor.lightGray.cgColor
+
+        countries += [
+            "estonia",
+            "france",
+            "germany",
+            "ireland",
+            "italy",
+            "monaco",
+            "nigeria",
+            "poland",
+            "russia",
+            "spain",
+            "uk",
+            "us"
+        ]
+    }
+    
+    func askQuestion() {
+        firstFlag.setImage(UIImage(named: countries[0]), for: .normal)
+        secondFlag.setImage(UIImage(named: countries[1]), for: .normal)
+        thirdFlag.setImage(UIImage(named: countries[2]), for: .normal)
     }
 
 
